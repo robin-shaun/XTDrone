897559e9fc80ee7fe9980817daa090821f3c876c
Robin Shaun <robin_shaun@foxmail.com>
2020-05-20 13:30:28 +0800

    更新yolo

--- control/yolo_human_tracking.py
+++ control/yolo_human_tracking.py
@@ -1,40 +1,44 @@
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 import sys 
 sys.path.append('/home/robin/catkin_ws/devel/lib/python2.7/dist-packages')
 from darknet_ros_msgs.msg import BoundingBoxes
 import time
 import math
-twist = Twist()
+
 def darknet_callback(data):
+    global twist, cmd
     for target in data.bounding_boxes:
-        print(target.id)
         if(target.id==0):
+            print('find human')
             x_error=y_center-(target.ymax+target.ymin)/2
             y_error=x_center-(target.xmax+target.xmin)/2
             twist.linear.x = Kp_linear*x_error
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
             twist.angular.z = Kp_angular*math.atan(y_error/x_error)
+            cmd = ''
 
         else:
             twist.linear.x = 0.0
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = 0.0         
-
-Kp_linear=0.05
-Kp_angular=0.2/math.pi
-x_center=752/2
-y_center=480/2
-rospy.init_node('yolo_human_tracking')
-rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, darknet_callback)
-pub = rospy.Publisher('/xtdrone/cmd_vel_flu', Twist, queue_size=10)
-rate = rospy.Rate(60) 
-while(True):
-    rate.sleep()
-    pub.publish(twist)
+            twist.angular.z = 0.0
+            cmd = 'HOVER'
+                
+            
+if __name__ == "__main__":  
+    twist = Twist()
+    cmd = String()
+    Kp_linear=0.05
+    Kp_angular=0.2/math.pi
+    x_center=752/2
+    y_center=480/2
+    vehicle_type = sys.argv[1]
+    rospy.init_node('yolo_human_tracking')
+    rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, darknet_callback)
+    vel_pub = rospy.Publisher('/xtdrone/'+vehicle_type+'_0/cmd_vel_flu', Twist, queue_size=2)
+    cmd_pub = rospy.Publisher('/xtdrone/'+vehicle_type+'_0/cmd', String, queue_size=2)
+    rate = rospy.Rate(60) 
+    while(True):
+        rate.sleep()
+        vel_pub.publish(twist)
+        cmd_pub.publish(cmd)
+        

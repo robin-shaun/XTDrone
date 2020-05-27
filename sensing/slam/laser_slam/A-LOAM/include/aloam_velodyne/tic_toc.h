d953d6b66b352758e70f2ee6b051d131ba4426a4
Robin Shaun <robin_shaun@foxmail.com>
2020-05-27 20:31:20 +0800

    加入aloam

---
+++ sensing/slam/laser_slam/A-LOAM/include/aloam_velodyne/tic_toc.h
@@ -0,0 +1,32 @@
+// Author:   Tong Qin               qintonguav@gmail.com
+// 	         Shaozu Cao 		    saozu.cao@connect.ust.hk
+
+#pragma once
+
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+
+class TicToc
+{
+  public:
+    TicToc()
+    {
+        tic();
+    }
+
+    void tic()
+    {
+        start = std::chrono::system_clock::now();
+    }
+
+    double toc()
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        return elapsed_seconds.count() * 1000;
+    }
+
+  private:
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+};

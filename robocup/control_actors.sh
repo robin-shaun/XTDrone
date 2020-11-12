81c33598fabe8e9b391c220c339bb12397202d53
Your Name <robin_shaun@foxmail.com>
2020-11-12 16:26:42 +0800

    modify actor control

---
+++ robocup/control_actors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python control_actor.py 0 &
+python control_actor.py 1 &
+python control_actor.py 2 &
+python control_actor.py 3 &
+python control_actor.py 4 &
+python control_actor.py 5
+

397bfc648d05f56e303de571fa250570db23e8cc
Your Name <robin_shaun@foxmail.com>
2020-11-22 09:59:29 +0800

    modify path for VINS

--- sensing/slam/vio/xtdrone_vins_run.sh
+++ sensing/slam/vio/xtdrone_vins_run.sh
@@ -1,2 +1,2 @@
-rosrun vins vins_node /home/robin/catkin_ws/src/VINS-Fusion/config/xtdrone_sitl/px4_sitl_stereo_imu_config.yaml &
+rosrun vins vins_node ~/catkin_ws/src/VINS-Fusion/config/xtdrone_sitl/px4_sitl_stereo_imu_config.yaml &
 roslaunch vins vins_rviz.launch

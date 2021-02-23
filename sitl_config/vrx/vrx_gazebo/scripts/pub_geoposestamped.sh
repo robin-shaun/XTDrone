7ba9b57abe78d8d1a824fe602f0adeced8c074f8
running-mars <guanzhengcn@foxmail.com>
2021-02-23 14:40:47 +0800

    remove needless things

--- sitl_config/vrx/vrx_gazebo/scripts/pub_geoposestamped.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Utility script for publishing landmark identification and localization
-
-rostopic pub -r 1 /vrx/perception/landmark geographic_msgs/GeoPoseStamped '{header: {stamp: now, frame_id: "red_mark"}, pose: {position: {latitude: 21.30996, longitude: -157.8901, altitude: 0.0}}}'
-
-#rostopic pub -r 1 /vrx/perception/landmark geographic_msgs/GeoPoseStamped '{header: {stamp: now, frame_id: "black_totem"}, pose: {position: {latitude: 21.30996, longitude: -157.8901, altitude: 0.0}}}'
--- sitl_config/vrx/wamv_gazebo/README.md
+++
@@ -1,2 +0,0 @@
-# wamv_gazebo
-Provides templates and examples for integrating the WAM-V model into gazebo with dynamics, sensors, etc.

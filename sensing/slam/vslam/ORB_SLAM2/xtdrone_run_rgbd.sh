a981395e40adb4b68a4676bb4a37d02699514478
robin_shaun <robin_shaun@foxmail.com>
2021-03-02 15:06:57 +0800

    modify camera_model

--- sensing/slam/vslam/ORB_SLAM2/xtdrone_run_rgbd.sh
+++ sensing/slam/vslam/ORB_SLAM2/xtdrone_run_rgbd.sh
@@ -1,1 +1,1 @@
-rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.txt Examples/RGB-D/px4_sitl.yaml
+rosrun ORB_SLAM2 RGBD ~/catkin_ws/src/ORB_SLAM2/Vocabulary/ORBvoc.txt ~/catkin_ws/src/ORB_SLAM2/Examples/RGB-D/px4_sitl.yaml
--- sensing/slam/vslam/ORB_SLAM2/xtdrone_run_stereo.sh
+++ sensing/slam/vslam/ORB_SLAM2/xtdrone_run_stereo.sh
@@ -1,1 +1,1 @@
-rosrun ORB_SLAM2 Stereo Vocabulary/ORBvoc.txt Examples/Stereo/px4_sitl.yaml true
+rosrun ORB_SLAM2 Stereo ~/catkin_ws/src/ORB_SLAM2/Vocabulary/ORBvoc.txt ~/catkin_ws/src/ORB_SLAM2/Examples/Stereo/px4_sitl.yaml true

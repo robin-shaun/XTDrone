30a599f411d8d9be26d869f54377fa27231cd6f7
robin_shaun <robin_shaun@foxmail.com>
2021-06-06 13:03:57 +0800

    add control_gazebo_models

---
+++ control/control_gazebo_models.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import rospy
+from gazebo_msgs.msg import ModelState
+
+def pose_publisher():
+    pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=10)
+    pose_msg = ModelState()
+    pose_msg.model_name = 'iris_0'
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+           pose_msg.pose.position.x = 0
+           pose_msg.pose.position.y = 0
+           pose_msg.pose.position.z = 1
+           pub.publish(pose_msg)
+           rate.sleep()
+
+if __name__ == '__main__':
+      rospy.init_node('pose_publisher')
+      try:
+          pose_publisher()
+      except rospy.ROSInterruptException:
+          pass

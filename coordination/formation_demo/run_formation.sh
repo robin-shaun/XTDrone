9cc41db5417f124cd06e170cad1107db4ae47fe7
robin_shaun <robin_shaun@foxmail.com>
2021-11-29 23:53:32 +0800

    modify run_formation.sh

--- coordination/formation_demo/run_formation.sh
+++ coordination/formation_demo/run_formation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python2.7 leader.py $1 $2 &
+python leader.py $1 $2 &
 uav_num=1
 while(( $uav_num< $2 )) 
 do
-    python2.7 follower_consensus.py $1 $uav_num $2&
+    python follower.py $1 $uav_num $2&
     let "uav_num++"
 done
--- coordination/formation_demo/stop_formation.sh
+++ coordination/formation_demo/stop_formation.sh
@@ -1,2 +1,2 @@
-kill -9 $(ps -ef|grep follower_consensus.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+kill -9 $(ps -ef|grep follower.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
 kill -9 $(ps -ef|grep leader.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')

245b25ba08426a99f717dbe87feb7d38521709d1
robin_shaun <robin_shaun@foxmail.com>
2021-12-14 16:10:02 +0800

    delete simplified_simulator

--- coordination/catch_actors/run_catching.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python catching_uavs.py 1 &
-python catching_uavs.py 2 &
-python catching_uavs.py 3 &
-python catching_uavs.py 4 &
-python catching_uavs.py 5
-
--- coordination/catch_actors/stop_actor.sh
+++
@@ -1,1 +0,0 @@
-kill -9 $(ps -ef|grep catching_uavs.py|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')

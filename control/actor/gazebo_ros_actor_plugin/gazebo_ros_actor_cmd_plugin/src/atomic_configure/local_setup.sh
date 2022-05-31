9bf82d6b2fa81018238a57a45b7ca734771b15c5
robin_shaun <robin_shaun@foxmail.com>
2022-05-31 15:55:23 +0800

    update control_gazebo_models

--- control/actor/gazebo_ros_actor_plugin/gazebo_ros_actor_cmd_plugin/src/atomic_configure/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/robocup/XTDrone/control/actor/gazebo_ros_actor_plugin/gazebo_ros_actor_cmd_plugin/src/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
--- control/control_gazebo_models.py
+++ control/control_gazebo_models.py
@@ -5,7 +5,9 @@ import sys
 
 def pose_publisher():
     vehicle_type = sys.argv[1]
-    vehicle_num = int(sys.argv[2])
+    row_num = int(sys.argv[2])
+    column_num = int(sys.argv[3])
+    vehicle_num = row_num * column_num
     pub = rospy.Publisher('gazebo/set_model_states', ModelStates, queue_size=1)
     poses_msg = ModelStates()
     poses_msg.name = [None] * vehicle_num
@@ -13,14 +15,15 @@ def pose_publisher():
     poses_msg.twist = [Twist()for i in range(vehicle_num)]
     for i in range(vehicle_num):
         poses_msg.name[i] = vehicle_type + '_' + str(i)
+        poses_msg.pose[i].position.x  = i / row_num
+        poses_msg.pose[i].position.y = i % row_num
+        poses_msg.pose[i].position.z = 3
     f = 10
     v = 0.5
     rate = rospy.Rate(f)
     while not rospy.is_shutdown():
         for i in range(vehicle_num):
             poses_msg.pose[i].position.x = poses_msg.pose[i].position.x + 0.5 / f
-            poses_msg.pose[i].position.y = i
-            poses_msg.pose[i].position.z = 3
         pub.publish(poses_msg)
         rate.sleep()
 if __name__ == '__main__':

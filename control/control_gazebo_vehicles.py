774d4b5d64bfec1128c494abdd8dbeb92de2a6dd
robin_shaun <robin_shaun@foxmail.com>
2022-08-06 18:57:18 +0800

    add control_gazebo_models

--- control/control_gazebo_vehicles.py
+++ control/control_gazebo_vehicles.py
@@ -1,26 +1,28 @@
 import rospy
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Pose, Twist
+import sys
 
 def pose_publisher():
+    vehicle_type = sys.argv[1]
+    row_num = int(sys.argv[2])
+    column_num = int(sys.argv[3])
+    vehicle_num = row_num * column_num
     pub = rospy.Publisher('gazebo/set_model_states', ModelStates, queue_size=1)
     poses_msg = ModelStates()
-    poses_msg.name = [None] * 2
-    poses_msg.pose = [Pose()for i in range(2)]
-    poses_msg.twist = [Twist()for i in range(2)]
-    poses_msg.name[0] = 'rover_static'
-    poses_msg.pose[0].position.x  = 70
-    poses_msg.pose[0].position.y = 1
-    poses_msg.pose[0].position.z = 0
-    poses_msg.name[1] = 'rover_static_clone'
-    poses_msg.pose[1].position.x  = 70
-    poses_msg.pose[1].position.y = 5
-    poses_msg.pose[1].position.z = 0
+    poses_msg.name = [None] * vehicle_num
+    poses_msg.pose = [Pose()for i in range(vehicle_num)]
+    poses_msg.twist = [Twist()for i in range(vehicle_num)]
+    for i in range(vehicle_num):
+        poses_msg.name[i] = vehicle_type + '_' + str(i)
+        poses_msg.pose[i].position.x  = i / row_num
+        poses_msg.pose[i].position.y = i % row_num
+        poses_msg.pose[i].position.z = 3
     f = 20
     v = 0.5
     rate = rospy.Rate(f)
     while not rospy.is_shutdown():
-        for i in range(2):
+        for i in range(vehicle_num):
             poses_msg.pose[i].position.x = poses_msg.pose[i].position.x + v / f
         pub.publish(poses_msg)
         rate.sleep()

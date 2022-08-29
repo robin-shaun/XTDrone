ae68081a668eb29d4b3dd3493696fa595797e562
xinyuHU <13319200380@163.com>
2022-08-29 22:34:55 +0800

    update score_cal

--- robocup/score_cal.py
+++ robocup/score_cal.py
@@ -20,14 +20,10 @@ actor_id_dict = {'green':[0], 'blue':[1], 'brown':[2], 'white':[3], 'red':[4,5]}
 #     if rospy.get_time() - start_time > 60 and not msg.armed and fall_detect[vehicle_id] == 0:
 #         fall_detect[vehicle_id] = 1
 #         print('UAV_' + str(vehicle_id) + ': falled')
-        
-
 
 def actor_info_callback(msg):
     global target_finish, start_time, score, count_flag, left_actors, actors_pos, find_time, topic_arrive_time
     actor_id = actor_id_dict[msg.cls]
-    if msg.cls == 'red':
-        red_cnt = 0
     for i in actor_id:
         if i not in left_actors:
             continue
@@ -39,11 +35,84 @@ def actor_info_callback(msg):
                 find_time[i] = rospy.get_time()
                 print("find actor_"+str(i))
             elif rospy.get_time() - find_time[i] >= 15:
-                target_finish += 1
                 del_model('actor_'+str(i))
                 left_actors.remove(i)
                 print('actor_'+str(i)+' is OK')
                 print('Time usage:', time_usage)
+                target_finish = 6-len(left_actors)
+                # calculate score
+                if target_finish == 6:
+                    score = (1200 - time_usage) - sensor_cost * 3e-3 
+                    print('score:',score)
+                    print("Mission finished")
+                    while True:
+                        pass
+                else:
+                    score = (2 + target_finish) * 60 - sensor_cost * 3e-3
+                    print('score:',score)
+                    break        
+        else:
+            count_flag[i] = False
+
+def actor_info1_callback(msg):
+    global target_finish, start_time, score, count_flag, left_actors, actors_pos, find_time, topic_arrive_time,flag_1,flag_2
+    red_cnt = 0
+    actor_id = actor_id_dict[msg.cls]
+    for i in actor_id:
+        if i not in left_actors:
+            continue
+        topic_arrive_interval = rospy.get_time() - topic_arrive_time[i]
+        topic_arrive_time[i] = rospy.get_time()
+        if (msg.x-actors_pos[i].x)**2+(msg.y-actors_pos[i].y)**2<err_threshold**2 and topic_arrive_interval<1:
+            if not count_flag[i]:
+                count_flag[i] = True
+                find_time[i] = rospy.get_time()
+                print("find actor_"+str(i))
+                flag_1=i
+            elif rospy.get_time() - find_time[i] >= 15:
+                del_model('actor_'+str(i))
+                left_actors.remove(i)
+                print('actor_'+str(i)+' is OK')
+                print('Time usage:', time_usage)
+                target_finish = 6-len(left_actors)
+                # calculate score
+                if target_finish == 6:
+                    score = (1200 - time_usage) - sensor_cost * 3e-3 
+                    print('score:',score)
+                    print("Mission finished")
+                    while True:
+                        pass
+                else:
+                    score = (2 + target_finish) * 60 - sensor_cost * 3e-3
+                    print('score:',score)
+                    break        
+        else:
+            red_cnt+=1
+            if red_cnt == 2 and not flag_1==0:
+                count_flag[flag_1] = False
+                flag_1=0
+
+def actor_info2_callback(msg):
+    global target_finish, start_time, score, count_flag, left_actors, actors_pos, find_time, topic_arrive_time,flag_1,flag_2
+    red_cnt = 0
+    actor_id = actor_id_dict[msg.cls]
+    for i in actor_id:
+        if i not in left_actors:
+            continue
+        topic_arrive_interval = rospy.get_time() - topic_arrive_time[i]
+        topic_arrive_time[i] = rospy.get_time()
+        if (msg.x-actors_pos[i].x)**2+(msg.y-actors_pos[i].y)**2<err_threshold**2 and topic_arrive_interval<1:
+            if not count_flag[i]:
+                count_flag[i] = True
+                find_time[i] = rospy.get_time()
+                print("find actor_"+str(i))
+                flag_2=i
+            elif rospy.get_time() - find_time[i] >= 15:
+                del_model('actor_'+str(i))
+                left_actors.remove(i)
+                print('actor_'+str(i)+' is OK')
+                print('Time usage:', time_usage)
+                target_finish = 6-len(left_actors)
                  
                 # calculate score
                 if target_finish == 6:
@@ -57,13 +126,10 @@ def actor_info_callback(msg):
                     print('score:',score)
                     break        
         else:
-            if msg.cls == 'red':
-                red_cnt += 1
-                if red_cnt == 2:
-                    count_flag[i] = False
-                    count_flag[i-1] = False
-            else:
-                count_flag[i] = False
+            red_cnt+=1
+            if red_cnt == 2 and not flag_2==1:
+                count_flag[flag_2] = False
+                flag_2=1
 
 if __name__ == "__main__":
     left_actors = range(actor_num)
@@ -71,12 +137,13 @@ if __name__ == "__main__":
     count_flag = [False] * actor_num
     topic_arrive_time = [0.0] * actor_num
     find_time = [0.0] * actor_num
+    flag_1=0
+    flag_2=1
     rospy.init_node('score_cal')
     time.sleep(1)
     start_time = rospy.get_time()
     del_model = rospy.ServiceProxy("/gazebo/delete_model",DeleteModel)
     get_model_state = rospy.ServiceProxy("/gazebo/get_model_state",GetModelState) 
-
     score_pub = rospy.Publisher("/score",Int16,queue_size=1) 
     time_usage_pub = rospy.Publisher("/time_usage",Int16,queue_size=1) 
     left_actors_pub = rospy.Publisher("/left_actors",String,queue_size=1)
@@ -84,8 +151,8 @@ if __name__ == "__main__":
     actor_green_sub = rospy.Subscriber("/actor_green_info",ActorInfo,actor_info_callback,queue_size=1)
     actor_white_sub = rospy.Subscriber("/actor_white_info",ActorInfo,actor_info_callback,queue_size=1)
     actor_brown_sub = rospy.Subscriber("/actor_brown_info",ActorInfo,actor_info_callback,queue_size=1)
-    actor_red1_sub = rospy.Subscriber("/actor_red1_info",ActorInfo,actor_info_callback,queue_size=1)
-    actor_red2_sub = rospy.Subscriber("/actor_red2_info",ActorInfo,actor_info_callback,queue_size=1)
+    actor_red1_sub = rospy.Subscriber("/actor_red1_info",ActorInfo,actor_info1_callback,queue_size=1)
+    actor_red2_sub = rospy.Subscriber("/actor_red2_info",ActorInfo,actor_info2_callback,queue_size=1)
     # state_sub = [None] * 6
     # for vehicle_id in range(uav_num):
     #     state_sub[vehicle_id] = rospy.Subscriber('/typhoon_h480_'+str(vehicle_id)+'/mavros/state', State, state_callback, vehicle_id)

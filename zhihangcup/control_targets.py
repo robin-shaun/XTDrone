623508c8201296c79f0a92eafd0cfba138338cdb
robin_shaun <robin_shaun@foxmail.com>
2022-12-06 20:37:52 +0800

    update

--- zhihangcup/control_targets.py
+++ zhihangcup/control_targets.py
@@ -1,9 +1,13 @@
 import rospy
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Pose, Twist
+from std_msgs.msg import Float32
+from gazebo_msgs.srv import GetLinkState
 
 def pose_publisher():
-    pub = rospy.Publisher('gazebo/set_model_states', ModelStates, queue_size=1)
+    model_state_pub = rospy.Publisher('gazebo/set_model_states', ModelStates, queue_size=1)
+    distance_pub = rospy.Publisher('/gazebo/distance', Float32, queue_size=1)
+    relative_pose_pub = rospy.Publisher("/gazebo/relative_pose", Pose, queue_size=1)
     poses_msg = ModelStates()
     poses_msg.name = [None] * 3
     poses_msg.pose = [Pose()for i in range(3)]
@@ -14,21 +18,21 @@ def pose_publisher():
 
     poses_msg.pose[0].position.x  = -8
     poses_msg.pose[0].position.y = -8.5
-    poses_msg.pose[0].position.z = 1.5
+    poses_msg.pose[0].position.z = 1.5 - 0.45
     
     poses_msg.pose[1].position.x  = 11
     poses_msg.pose[1].position.y = 2.5
-    poses_msg.pose[1].position.z = 1.5
+    poses_msg.pose[1].position.z = 1.5 - 0.45
 
     poses_msg.pose[2].position.x  = 13.5
     poses_msg.pose[2].position.y = -8
-    poses_msg.pose[2].position.z = 1.5
+    poses_msg.pose[2].position.z = 1.5 - 0.45
     poses_msg.pose[2].orientation.z = 0.707106
     poses_msg.pose[2].orientation.w = 0.707106
 
     f = 10.0
     v = 0.0
-    a = 0.5
+    a = 0.05
     i = 0
     period_1 = 2
     period_2 = period_1 + 4
@@ -40,11 +44,11 @@ def pose_publisher():
         if (i%(period_total*f)<period_1*f):
             v = v + a/f
         elif (i%(period_total*f)<period_2*f):
-            v = 1.0
+            v = 0.1
         elif (i%(period_total*f)<period_3*f):
             v = v - a/f
         elif (i%(period_total*f)<period_4*f):
-            v = -1.0
+            v = -0.1
         else:
             v = v + a/f
         poses_msg.pose[0].position.y = poses_msg.pose[0].position.y + v / f
@@ -56,10 +60,20 @@ def pose_publisher():
             poses_msg.pose[2].position.x = poses_msg.pose[2].position.x + v / f
         
 
-        pub.publish(poses_msg)
+        model_state_pub.publish(poses_msg)
         i = i + 1
+        try:
+            response1 = get_link_state('target_green::link','iris_0::realsense_camera::link')
+            response2 = get_link_state('iris_0::realsense_camera::link', 'target_green::link')
+        except:
+            continue
+        distance = abs(response1.link_state.pose.position.x) * 100
+        distance_pub.publish(distance)
+        relative_pose = response2.link_state.pose
+        relative_pose_pub.publish(relative_pose)
         rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('control_targets')
+    get_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
     pose_publisher()
--- zhihangcup/score2_realtime.py
+++ zhihangcup/score2_realtime.py
@@ -2,20 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 import rospy
-from std_msgs.msg import Int16, Bool
-from gazebo_msgs.srv import GetModelState, GetLinkState
-from sensor_msgs.msg import Image
+from std_msgs.msg import Bool, Float32
+from geometry_msgs.msg import Pose
 import time
 from mavros_msgs.msg import State
-import sys
-from cv_bridge import CvBridge
-bridge = CvBridge()
-import cv2
+import os
 import numpy as np
 from scipy.spatial.transform import Rotation
 import math
-img_width = 1280
-img_height = 720
 
 def mavros_state_callback(msg):
     global armed
@@ -25,64 +19,67 @@ def rendezvous_callback(msg):
     global rendezvous
     rendezvous = msg.data
 
+def distance_callback(msg):
+    global distance
+    distance = msg.data
+
+def relative_pose_callback(msg):
+    global rendezvous, score_sum, score_cnt, start_time, first_score, rendezvous_start_time, score1
+    if(rendezvous):
+        x0 = msg.position.x
+        y0 = msg.position.y
+        z0 = msg.position.z
+        Rq = [msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w]
+        rot = Rotation.from_quat(Rq)
+        euler = rot.as_euler('zyx', degrees=False)
+        y = -x0 * math.tan(euler[0]) + y0
+        z = -x0 * math.tan(euler[1]) / math.cos(euler[0]) + z0
+        r = (y**2+z**2)**0.5
+        ring = 10 - math.floor(r / 0.0225)
+        if(ring < 0):
+            ring = 0
+        if(first_score):
+            score1 = (ring**2 + (10-np.min([10, abs(distance - 30)]))**2) / 4
+            score_sum = score1
+            score_cnt =  score_cnt + 1
+            print("Score1: %.3f"%score1)
+            first_score = False
+            rendezvous_start_time = rospy.get_time()
+        else:
+            score_sum = score_sum + (ring**2 + (10-np.min([10, abs(distance - 30)]))**2) / 4
+            print("Score sum: %.3f"%score_sum)
+            score_cnt =  score_cnt + 1
+            print("Score cnt: %d"%score_cnt)
+            if(rospy.get_time()-rendezvous_start_time>10):
+                score2 = score_sum / score_cnt
+                print("Score2: %.3f"%score2) 
+                score = score1 + score2
+                print("Score: %.3f"%score)
+                rendezvous = False 
+    time_usage = rospy.get_time() - start_time
+    if(time_usage > 1200):
+        print("Time out, score: 0")
+        os._exit(0) 
+    if(time_usage > 30 and not armed):
+        print("Mission finish, time usage: %.3f"%time_usage) 
+        os._exit(0)    
+
 if __name__ == "__main__":
     rospy.init_node('score_cal')
     time.sleep(1)
     armed = False
     rendezvous = False
     first_score = True
-    mavros_sub = rospy.Subscriber("/iris_0/mavros/state", State, mavros_state_callback)
-    rendezvous_sub = rospy.Subscriber("/rendezvous", Bool, rendezvous_callback)
-    get_model_state = rospy.ServiceProxy("/gazebo/get_model_state",GetModelState) 
-    get_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-    target_finish = 0
-    score1 = 0.0
-    score2 = 0.0
+    rendezvous_start_time = rospy.get_time()
+    mavros_sub = rospy.Subscriber("/iris_0/mavros/state", State, mavros_state_callback,queue_size=1)
+    rendezvous_sub = rospy.Subscriber("/rendezvous", Bool, rendezvous_callback,queue_size=1)
+    distance_sub = rospy.Subscriber("/gazebo/distance", Float32, distance_callback,queue_size=1)
+    relative_pose_sub = rospy.Subscriber("/gazebo/relative_pose", Pose, relative_pose_callback,queue_size=1)
+    distance = 100.0
     score_sum = 0.0
-    total_score = 0.0
     score_cnt = 0
+    score1 = 0
     rate = rospy.Rate(20)
     start_time = rospy.get_time()
     while not rospy.is_shutdown():
-        if(rendezvous):
-            try:
-                response1 = get_link_state('target_green::link','iris_0::realsense_camera::link')
-                response2 = get_link_state('iris_0::realsense_camera::link', 'target_green::link')
-            except:
-                print("Gazebo model state service call failed")
-            distance = abs(response1.link_state.pose.position.x) * 100
-            x0 = response2.link_state.pose.position.x
-            y0 = response2.link_state.pose.position.y
-            z0 = response2.link_state.pose.position.z
-            Rq = [response2.link_state.pose.orientation.x,response2.link_state.pose.orientation.y,response2.link_state.pose.orientation.z,response2.link_state.pose.orientation.w]
-            rot = Rotation.from_quat(Rq)
-            euler = rot.as_euler('zyx', degrees=False)
-            y = -x0 * math.tan(euler[0]) + y0
-            z = -x0 * math.tan(euler[1]) / math.cos(euler[0]) + z0
-            r = (y**2+z**2)**0.5
-            ring = 10 - math.floor(r / 0.0225)
-            if(ring < 0):
-                ring = 0
-            if(first_score):
-                score1 = (ring**2 + (10-np.min([10, abs(distance - 30)]))**2) / 4
-                score_sum = score1
-                score_cnt =  score_cnt + 1
-                print("Score1: %.3f"%score1)
-                first_score = False
-                start_time = rospy.get_time()
-            else:
-                score_sum = score_sum + (ring**2 + (10-np.min([10, abs(distance - 30)]))**2) / 4
-                score_cnt =  score_cnt + 1
-                if(rospy.get_time()-start_time>10):
-                    score2 = score_sum / score_cnt
-                    print("Score2: %.3f"%score2) 
-                    score = score1 + score2
-                    print("Score: %.3f"%score)
-                    rendezvous = False 
-        time_usage = rospy.get_time() - start_time
-        if(time_usage > 1200):
-            print("Time out, score: 0")
-            sys.exit(0) 
-        if(time_usage > 30 and not armed):
-            print("Mission finish, time usage: %.3f"%time_usage) 
-            sys.exit(0)      
+        rate.sleep()  

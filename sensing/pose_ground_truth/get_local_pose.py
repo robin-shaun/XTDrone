d9c5e526ebd86fdd9e359bcb82afea0923675d2c
卓安 <zhuoan@stu.pku.edu.cn>
2024-10-30 11:10:53 +0800

    Update get_local_pose.py to publish vision_odom/odom for each vehicle

--- sensing/pose_ground_truth/get_local_pose.py
+++ sensing/pose_ground_truth/get_local_pose.py
@@ -1,13 +1,16 @@
 import rospy
 from geometry_msgs.msg import PoseStamped, Vector3Stamped
+from nav_msgs.msg import Odometry
 import sys
 from gazebo_msgs.msg import ModelStates
 
 vehicle_type = sys.argv[1]
 vehicle_num = int(sys.argv[2])
 multi_pose_pub = [None]*vehicle_num
+multi_odom_pub = [None]*vehicle_num
 multi_speed_pub = [None]*vehicle_num
 multi_local_pose = [PoseStamped() for i in range(vehicle_num)]
+multi_odom = [Odometry() for i in range(vehicle_num)]
 multi_speed = [Vector3Stamped() for i in range(vehicle_num)]
 
 def gazebo_model_state_callback(msg):
@@ -16,6 +19,11 @@ def gazebo_model_state_callback(msg):
         multi_local_pose[vehicle_id].header.stamp = rospy.Time().now()
         multi_local_pose[vehicle_id].header.frame_id = 'map'
         multi_local_pose[vehicle_id].pose = msg.pose[id]
+        multi_odom[vehicle_id].header.stamp = rospy.Time().now()
+        multi_odom[vehicle_id].header.frame_id = 'map'
+        multi_odom[vehicle_id].child_frame_id = "base_link"
+        multi_odom[vehicle_id].pose.pose = msg.pose[id]
+        multi_odom[vehicle_id].twist.twist = msg.twist[id]
         multi_speed[vehicle_id].header.stamp = rospy.Time().now()
         multi_speed[vehicle_id].header.frame_id = 'map'
         multi_speed[vehicle_id].vector = msg.twist[id]
@@ -25,6 +33,7 @@ if __name__ == '__main__':
     gazebo_model_state_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, gazebo_model_state_callback,queue_size=1)
     for i in range(vehicle_num):
         multi_pose_pub[i] = rospy.Publisher(vehicle_type+'_'+str(i)+'/mavros/vision_pose/pose', PoseStamped, queue_size=1)
+        multi_odom_pub[i] = rospy.Publisher(vehicle_type+'_'+str(i)+'/mavros/vision_odom/odom', Odometry, queue_size=1)
         multi_speed_pub[i] = rospy.Publisher(vehicle_type+'_'+str(i)+'/mavros/vision_speed/speed', Vector3Stamped, queue_size=1)
         print("Get " + vehicle_type + "_" + str(i) + " groundtruth pose")
     rate = rospy.Rate(30)
@@ -32,6 +41,7 @@ if __name__ == '__main__':
         for i in range(vehicle_num):
             multi_pose_pub[i].publish(multi_local_pose[i])
             multi_speed_pub[i].publish(multi_speed[i])
+            multi_odom_pub[i].publish(multi_odom[i])
         try:
             rate.sleep()
         except:
